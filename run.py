901724ca864002525f308df60b49243f8ef66fa1
dave <davete@yopmail.com>
2015-08-02 12:46:09 +0000

    add more spiders to run.py

--- run.py
+++ run.py
@@ -1,16 +1,32 @@
+#! /usr/bin/env python
+
 from twisted.internet import reactor
-import scrapy
+
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
+from scrapy.conf import settings
 
 from scrapyscrappers.spiders.simplyhired import SimplyhiredSpider
 from scrapyscrappers.spiders.usajobs import UsajobsSpider
+from scrapyscrappers.spiders.careerbuilder import CareerbuilderSpider
+from scrapyscrappers.spiders.clearedconnections import ClearedconnectionsSpider
+from scrapyscrappers.spiders.clearedjobs import ClearedjobsSpider
+from scrapyscrappers.spiders.glassdoor import GlassdoorSpider
+from scrapyscrappers.spiders.clearancejobs import ClearancejobsSpider
 
-configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
+
+configure_logging({'LOG_FORMAT': settings.get('LOG_FORMAT')})
 runner = CrawlerRunner()
 
 d = runner.crawl(SimplyhiredSpider)
 d = runner.crawl(UsajobsSpider)
+d = runner.crawl(CareerbuilderSpider)
+d = runner.crawl(ClearedconnectionsSpider)
+d = runner.crawl(ClearedjobsSpider)
+d = runner.crawl(GlassdoorSpider)
+d = runner.crawl(ClearancejobsSpider)
+
+d = runner.join()
 d.addBoth(lambda _: reactor.stop())
 reactor.run() # the script will block here until the crawling is finished
 
